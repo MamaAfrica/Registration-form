e5e2134a7d5285300166c0c5f25320455cff24b3
Miracle <nnabukomiracle@gmail.com>
2022-03-26 21:18:26 +0100

    changed zoho port"

--- regform/settings.py
+++ regform/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.zoho.com'
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'admin@graceafrica.xyz'
 EMAIL_HOST_PASSWORD = 'Mimigrace@11'
